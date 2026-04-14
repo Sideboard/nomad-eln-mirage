d588fcea46b672281cb13ad71b499f86825895e6
Sascha Klawohn <sascha.klawohn@physik.hu-berlin.de>
2026-04-14 11:34:56 +0200

    Add ELN Mirage classes from yaml schema (sintering)

--- src/nomad_eln_mirage/schema_packages/__init__.py
+++ src/nomad_eln_mirage/schema_packages/__init__.py
@@ -2,16 +2,16 @@ from nomad.config.models.plugins import SchemaPackageEntryPoint
 from pydantic import Field
 
 
-class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+class ELNMirageEntryPoint(SchemaPackageEntryPoint):
     parameter: int = Field(0, description='Custom configuration parameter')
 
     def load(self):
-        from nomad_eln_mirage.schema_packages.schema_package import m_package
+        from nomad_eln_mirage.schema_packages.eln_mirage import m_package
 
         return m_package
 
 
-schema_package_entry_point = NewSchemaPackageEntryPoint(
-    name='NewSchemaPackage',
-    description='New schema package entry point configuration.',
+eln_mirage = ELNMirageEntryPoint(
+    name='ELN Mirage',
+    description='Exemplatory schema plugin for testing ELNs',
 )
